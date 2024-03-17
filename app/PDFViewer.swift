619efef6fb6fe783a8f61cfe8e6d71cf56d4a324
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2024-03-17 22:32:15 +0900

    refactor : resources의 pdf 출력을 web pdf 로 변경
    경로 설정 문제로 인해서 웹의 pdf를 직접 가져와 저장해서 출력하는 방식으로 변경

--- app/PDFViewer.swift
+++ app/PDFViewer.swift
@@ -1,47 +1,57 @@
-
-//  File.swift
-//  app
-//
-//  Created by 김규형 on 3/16/24.
-//
-
-import Foundation
 import SwiftUI
 import PDFKit
 
-
-
-// PDF 번호와 파일명을 매칭하는 Dictionary
-let pdfMapping: [Int: Int] = [
-    1: 1,
-    997: 40
+// PDF 번호와 파일 URL을 매칭하는 Dictionary
+let pdfMapping: [Int: URL] = [
+    1: URL(string: "https://www.example.com/example1.pdf")!,
+    997: URL(string: "https://www.naju.go.kr/contents/9353/231030/citybus_100.pdf")!
 ]
 
 struct PDFViewer: View {
     let selectedPDFNumber: Int
     
+    @State private var pdfDocument: PDFDocument?
+
     var body: some View {
         VStack {
-            // 선택된 PDF 번호에 따라 해당하는 PDF를 표시
-            if let pdfFileName = pdfMapping[selectedPDFNumber],
-               let pdfURL = Bundle.main.url(forResource: "resources/timetablePDF/citybus_+\(pdfFileName)", withExtension: "pdf") {
-                PDFKitRepresentedView(url: pdfURL)
-                    .edgesIgnoringSafeArea(.all)
+            // PDF를 로드하는데 실패했을 때 에러 메시지를 표시합니다.
+            if pdfDocument == nil {
+                Text("PDF 파일을 로드하는 중입니다...")
             } else {
-                Text(String(selectedPDFNumber) + "PDF 파일을 찾을 수 없습니다.")
+                PDFKitRepresentedView(document: pdfDocument!)
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+        .onAppear {
+            // PDF 매핑에서 해당 PDF의 URL을 가져옵니다.
+            if let pdfURL = pdfMapping[selectedPDFNumber] {
+                // 외부 URL에서 직접 PDF를 로드합니다.
+                URLSession.shared.dataTask(with: pdfURL) { data, response, error in
+                    if let data = data, let document = PDFDocument(data: data) {
+                        DispatchQueue.main.async {
+                            self.pdfDocument = document
+                        }
+                    } else {
+                        print("Error: PDF 파일을 로드할 수 없습니다.")
+                    }
+                }.resume()
+            } else {
+                print("Error: PDF 매핑을 찾을 수 없습니다.")
             }
         }
     }
 }
 
 struct PDFKitRepresentedView: UIViewRepresentable {
-    var url: URL
+    let document: PDFDocument
 
     func makeUIView(context: Context) -> PDFView {
         let pdfView = PDFView()
-        pdfView.document = PDFDocument(url: url)
+        pdfView.document = document
         return pdfView
     }
 
-    func updateUIView(_ uiView: PDFView, context: Context) {}
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        uiView.document = document
+    }
 }
