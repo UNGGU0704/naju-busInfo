4120c8c5d66eb862c90e3e8d8bb75d71210bcbbc
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2024-06-25 15:05:20 +0900

    Feat : 버스 노선 정보 fetch 기능 추가(1) #11

--- app/DB.swift
+++ app/DB.swift
@@ -2,8 +2,8 @@ import UIKit
 import Foundation
 import CoreData
 
-struct DBApiResponse: Codable {
-    let bus_stops: [BusStop]
+struct DBApiResponse<T: Codable>: Codable {
+    let bus_stops: [T]
     let row_count: Int
     let admin :Int = 5
     private enum CodingKeys: String, CodingKey {
@@ -11,7 +11,7 @@ struct DBApiResponse: Codable {
         case row_count = "ROW_COUNT"
     }
 }
-
+    
 struct BusStop: Codable {
     let busStopName: String
     let arsID: Int
@@ -42,21 +42,53 @@ struct BusStop: Codable {
         latitude = try container.decode(Double.self, forKey: .latitude)
     }
 }
+
+struct BusRoute: Codable {
+    let dirDownName: String
+    let dirPass: String
+    let runInterval: String
+    let firstTime: String?
+    let lastTime: String?
+    let dirUpName: String
+    let lineID: Int
+    let lineKind: Int
+    let lineName: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case dirDownName = "DIR_DOWN_NAME"
+        case dirPass = "DIR_PASS"
+        case runInterval = "RUN_INTERVAL"
+        case firstTime = "FIRST_TIME"
+        case lastTime = "LAST_TIME"
+        case dirUpName = "DIR_UP_NAME"
+        case lineID = "LINE_ID"
+        case lineKind = "LINE_KIND"
+        case lineName = "LINE_NAME"
+    }
+}
+
+
+
 public func deleteExistingData() {
     let context = PersistenceController.shared.container.viewContext
-    let request: NSFetchRequest<Item> = Item.fetchRequest()
-    
-    if let items = try? context.fetch(request) {
-        for item in items {
-            context.delete(item)
+    let busStopRequest: NSFetchRequest<Item> = Item.fetchRequest()
+    let busRouteRequest: NSFetchRequest<LineInfo> = LineInfo.fetchRequest()
+
+    do {
+        let busStops = try context.fetch(busStopRequest)
+        for busStop in busStops {
+            context.delete(busStop)
         }
         
-        do {
-            try context.save()
-            context.processPendingChanges()
-        } catch {
-            print("Error deleting existing bus stop data: \(error)")
+        let busRoutes = try context.fetch(busRouteRequest)
+        for busRoute in busRoutes {
+            context.delete(busRoute)
         }
+
+        try context.save()
+        context.processPendingChanges()
+    } catch {
+        print("Error deleting existing data: \(error)")
     }
 }
 
@@ -91,7 +123,7 @@ public func fetchBusStopData() {
         
         context.performAndWait {
             do {
-                let DBapiResponse = try JSONDecoder().decode(DBApiResponse.self, from: data)
+                let DBapiResponse = try JSONDecoder().decode(DBApiResponse<BusStop>.self, from: data)
                 let busStopData = DBapiResponse.bus_stops
                 
                 for busStop in busStopData {
@@ -116,3 +148,50 @@ public func fetchBusStopData() {
         }
     }.resume()
 }
+
+public func fetchBusRouteData() {
+    print("Fetching bus route data...")
+    let context = PersistenceController.shared.container.viewContext
+    
+    guard let url = URL(string: "http://121.147.206.212/json/lineInfo") else {
+        print("Invalid URL")
+        return
+    }
+
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print("Error fetching data: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let data = data else {
+            print("No data received")
+            return
+        }
+        
+        context.performAndWait {
+            do {
+                let apiResponse = try JSONDecoder().decode(DBApiResponse<BusRoute>.self, from: data)
+                let busRouteData = apiResponse.bus_stops
+                
+                for route in busRouteData {
+                    let newRoute = LineInfo(context: context)
+                    newRoute.dirDownName = route.dirDownName
+                    newRoute.dirPass = route.dirPass
+                    newRoute.runInterval = route.runInterval
+                    newRoute.firstTime = route.firstTime ?? ""
+                    newRoute.lastTime = route.lastTime ?? ""
+                    newRoute.dirUpName = route.dirUpName
+                    newRoute.lineID = Int32(route.lineID)
+                    newRoute.lineKind = Int32(route.lineKind)
+                    newRoute.lineName = route.lineName
+                    
+                    try context.save()
+                }
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }.resume()
+}
+
